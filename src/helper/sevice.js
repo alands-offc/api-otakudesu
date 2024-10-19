dd7b41a311db95e91b3b399f00cd28d3c2259e78
alands-offc <alandikasaputra989@gmail.com>
2024-10-19 10:52:07 +0700

    Update sevice.js

--- src/helper/sevice.js
+++ src/helper/sevice.js
@@ -4,7 +4,6 @@ const { SocksProxyAgent } = require('socks-proxy-agent');
 async function fetchProxies() {
   try {
     const response = await axios.get('https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/protocols/socks5/data.json');
-                                     
     return response.data;
   } catch (error) {
     console.error('Error fetching proxy list:', error);
@@ -20,41 +19,49 @@ async function fetchDataUsingProxy(url) {
     return;
   }
 
-  // Ambil proxy acak dari daftar
+  // Select a random proxy from the list
   const randomProxy = proxies[Math.floor(Math.random() * proxies.length)];
-  const proxy = randomProxy.proxy;
+  const proxy = `socks5://${randomProxy.ip}:${randomProxy.port}`;
 
   console.log(`Using proxy: ${proxy}`);
 
   const agent = new SocksProxyAgent(proxy);
 
   try {
-    const result = await axios.get(url, { httpAgent: agent });
-    return result
+    const result = await axios.get(url, { 
+      httpAgent: agent, 
+      headers: {
+        'Cookie': '_ga=GA1.2.1714932629.1729306364; _gid=GA1.2.94538758.1729306364; _ga_025LZFQCB2=GS1.2.1729306365.1.1.1729309347.0.0.0'
+      }
+    });
+    return result;
   } catch (error) {
     console.error('Error fetching data:', error);
+    throw error;
   }
 }
 
-// Ganti dengan URL target yang ingin diakses
 const Service = {
-    fetchService: async (url, res) => {
-        try {
-            const response = await fetchDataUsingProxy(url)
-            return new Promise((resolve, reject) => {
-                if (response.status === 200) resolve(response)
-                reject(response)
-            })
-        } catch (error) {
-            res.json({
-                status: false,
-                code: 404,
-                message: "Bad Request",
-                error
-            })
-            throw error
+  fetchService: async (url, res) => {
+    try {
+      const response = await fetchDataUsingProxy(url);
+      return new Promise((resolve, reject) => {
+        if (response.status === 200) {
+          resolve(response.data);
+        } else {
+          reject(new Error(`Error: Status code ${response.status}`));
         }
+      });
+    } catch (error) {
+      res.json({
+        status: false,
+        code: 404,
+        message: "Bad Request",
+        error: error.message
+      });
+      throw error;
     }
-}
+  }
+};
 
-module.exports = Service
+module.exports = Service;
